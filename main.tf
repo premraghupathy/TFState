8cca4317924d5b6425a5153b83bbf1ec62e49cbe
Raghupathy <prem.raghupathy@gainwelltechnologies.com>
2024-10-22 15:18:51 -0700

    2nd rev

--- main.tf
+++ main.tf
@@ -46,3 +46,24 @@ resource "aws_dynamodb_table" "prem_tf_state_lock_dyna" {
     type = "S"
   }
 }
+
+terraform {
+  backend "prem_tf_state" {
+    bucket = "pprem-tf-state-s3"
+    key = "global/s3/terraform.tfstate"
+    region = "us-eat-2"
+
+    dynamodb_table = "prem-tf-state-lock-dyna"
+    encrypt = true
+  }
+}
+
+output "prem_tf_state_s3_arn" {
+  value = aws_s3_bucket.prem_tf_state_s3.arn
+  description = "Prem TF state S3 ARN"
+}
+
+output "prem_tf_state_dynamodb_table_name" {
+  value = aws_dynamodb_table.prem_tf_state_lock_dyna.name
+  description = "Prem TF state lock dynamodb table name"
+}
